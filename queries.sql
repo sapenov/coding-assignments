393bdb8fa47aa3b8df31c35542590878bca69300
Khazret Sapenov <sapenov@users.noreply.github.com>
2018-11-28 02:16:36 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -9,7 +9,9 @@ updated_date datetime,
 created_date datetime);
 
 /*populate sample schema with data */
-insert into recipes(recipe_id, recipe_name, description, ingredient, active, updated_date, created_date) values (1, 'pasta', 'Italian pasta', 'tomato sauce', true, '2018-01-09 10:00:57',  '2018-01-10 13:00:57'),
+insert into recipes(recipe_id, recipe_name, description, ingredient, active, updated_date, created_date) 
+values 
+(1, 'pasta', 'Italian pasta', 'tomato sauce', true, '2018-01-09 10:00:57',  '2018-01-10 13:00:57'),
 (1, 'pasta', null, 'cheese', true, '2018-01-09 10:10:57',  '2018-01-10 13:00:57'),
 (2, 'lasagna', 'layered lasagna', 'cheese', true, '2018-01-09 10:00:57',  '2018-01-10 13:00:57'),
 (2, 'lasagna', 'layered lasagna', 'blue cheese', false, '2018-01-09 10:00:57',  '2018-01-10 13:00:57');
