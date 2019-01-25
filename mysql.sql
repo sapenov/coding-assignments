dd5330432c7140d9f2e7d7967c565d180823049c
Khazret Sapenov <sapenov@users.noreply.github.com>
2019-01-25 03:08:50 -0500

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -27,3 +27,10 @@ on p.product_id = s.product_id
 group by brand_name
 having units >= 10 AND total > 20
 order by units desc;
+
+/* 5) For each store show % difference in sales between 2014 and 2015 */
+SELECT 
+  p1.store_id, ((p2.gross - p1.gross)/p2.gross * 100) AS 'Percentage Growth'
+FROM 
+    (SELECT store_id, sum(store_sales) AS gross FROM sales WHERE YEAR(transaction_date) = 2014 group by store_id) AS p1,
+    (SELECT store_id, sum(store_sales) AS gross FROM sales WHERE YEAR(transaction_date) = 2015 group by store_id) AS p2;
