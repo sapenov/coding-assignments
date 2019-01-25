c45c364a001e6df8c5e79d3c9e6449f51b93ae1e
Khazret Sapenov <sapenov@users.noreply.github.com>
2019-01-25 02:21:05 -0500

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,4 +1,4 @@
-Top 3 Products by sale
+/* Top 3 Products by sale */
 select p.product_name,  sum(s.store_sales) as gross 
 from sales as s 
 join products as p on p.product_id = s.product_id
@@ -6,7 +6,7 @@ group by product_name
 order by gross DESC
 limit 3;
 
-Count the number of products which sold more than 10 units
+/* Count the number of products which sold more than 10 units */
 select p.product_name, 
 sum(s.units_sold) as units,
 sum(p.price * s.units_sold) as total
