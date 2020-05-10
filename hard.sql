add68c15c7188016247d2922adc54e8026d03647
Khazret Sapenov <sapenov@users.noreply.github.com>
2020-05-09 17:04:13 -0700

    Update hard.sql

--- hard.sql
+++ hard.sql
@@ -16,7 +16,7 @@ WHERE
         )
 ;
 
-/* 190 ms
+/* 190 ms */
 select d.Name as Department, a. Name as Employee, a. Salary 
 from (
 select e.*, dense_rank() over (partition by DepartmentId order by Salary desc) as DeptPayRank 
@@ -25,13 +25,13 @@ from Employee e
 join Department d
 on a. DepartmentId = d. Id 
 where DeptPayRank <=3; 
-*/
 
-/* 270 ms
+
+/* 270 ms */
 select tD.Name as 'Department', tE1.Name as 'Employee', tE1.Salary from Employee as tE1
 Inner join Department as tD on tE1.DepartmentId = tD.Id
 Left join Employee as tE2 on tE1.DepartmentId = tE2.DepartmentId and tE1.Salary <= tE2.Salary
 group by tE1.Id
 having count(distinct tE2.Salary) <= 3
 order by tE1.DepartmentId, tE1.Salary desc
-*/
+
