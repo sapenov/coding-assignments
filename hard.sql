afe845ba0874accdb2400f0272d92637a4cf55b9
Khazret Sapenov <sapenov@users.noreply.github.com>
2020-05-09 17:03:20 -0700

    Create hard.sql

---
+++ hard.sql
@@ -0,0 +1,47 @@
+/*select e1.Name as 'Employee', e1.Salary
+from Employee e1
+where 3 >
+(
+    select count(distinct e2.Salary)
+    from Employee e2
+    where e2.Salary > e1.Salary
+)
+;*/
+
+/* 203 ms */
+SELECT
+    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+FROM
+    Employee e1
+        JOIN
+    Department d ON e1.DepartmentId = d.Id
+WHERE
+    3 > (SELECT
+            COUNT(DISTINCT e2.Salary)
+        FROM
+            Employee e2
+        WHERE
+            e2.Salary > e1.Salary
+                AND e1.DepartmentId = e2.DepartmentId
+        )
+;
+
+/* 190 ms
+select d.Name as Department, a. Name as Employee, a. Salary 
+from (
+select e.*, dense_rank() over (partition by DepartmentId order by Salary desc) as DeptPayRank 
+from Employee e 
+) a 
+join Department d
+on a. DepartmentId = d. Id 
+where DeptPayRank <=3; 
+*/
+
+/* 270 ms
+select tD.Name as 'Department', tE1.Name as 'Employee', tE1.Salary from Employee as tE1
+Inner join Department as tD on tE1.DepartmentId = tD.Id
+Left join Employee as tE2 on tE1.DepartmentId = tE2.DepartmentId and tE1.Salary <= tE2.Salary
+group by tE1.Id
+having count(distinct tE2.Salary) <= 3
+order by tE1.DepartmentId, tE1.Salary desc
+*/
