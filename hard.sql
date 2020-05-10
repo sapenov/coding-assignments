451b259ce463a3a212feb7e7ae9b4e34051ce53e
Khazret Sapenov <sapenov@users.noreply.github.com>
2020-05-10 15:34:18 -0700

    Update hard.sql

--- hard.sql
+++ hard.sql
@@ -55,7 +55,9 @@ order by tE1.DepartmentId, tE1.Salary desc
 
 -- Average Salary: Departments VS Company
 
-select department_salary.pay_month, department_id,
+select 
+department_salary.pay_month, 
+department_id,
 case
   when department_avg>company_avg then 'higher'
   when department_avg<company_avg then 'lower'
@@ -63,13 +65,25 @@ case
 end as comparison
 from
 (
-  select department_id, avg(amount) as department_avg, date_format(pay_date, '%Y-%m') as pay_month
-  from salary join employee on salary.employee_id = employee.employee_id
-  group by department_id, pay_month
+  select 
+    department_id, 
+    avg(amount) as department_avg, 
+    date_format(pay_date, '%Y-%m') as pay_month
+  from 
+  salary 
+  join employee 
+  on salary.employee_id = employee.employee_id
+  group by 
+  department_id, 
+  pay_month
 ) as department_salary
 join
 (
-  select avg(amount) as company_avg,  date_format(pay_date, '%Y-%m') as pay_month from salary group by date_format(pay_date, '%Y-%m')
+  select 
+    avg(amount) as company_avg,  
+    date_format(pay_date, '%Y-%m') as pay_month 
+    from salary 
+    group by date_format(pay_date, '%Y-%m')
 ) as company_salary
 on department_salary.pay_month = company_salary.pay_month
 ;
