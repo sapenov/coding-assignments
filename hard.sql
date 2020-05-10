68f6a5091bf5ec8bfdcc5c9fc38ff6e35f460be7
Khazret Sapenov <sapenov@users.noreply.github.com>
2020-05-09 17:09:35 -0700

    Update hard.sql

--- hard.sql
+++ hard.sql
@@ -1,10 +1,14 @@
 /* 203 ms */
 SELECT
-    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+    d.Name AS 'Department', 
+    e1.Name AS 'Employee', 
+    e1.Salary
 FROM
     Employee e1
-        JOIN
-    Department d ON e1.DepartmentId = d.Id
+JOIN
+    Department d 
+ON 
+    e1.DepartmentId = d.Id
 WHERE
     3 > (SELECT
             COUNT(DISTINCT e2.Salary)
@@ -13,8 +17,7 @@ WHERE
         WHERE
             e2.Salary > e1.Salary
                 AND e1.DepartmentId = e2.DepartmentId
-        )
-;
+        );
 
 /* 190 ms */
 select 
