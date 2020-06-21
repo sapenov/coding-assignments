0a72aea2a56f1242d3263bd53e2f9cf83f7ad32d
Khazret Sapenov <sapenov@users.noreply.github.com>
2020-06-21 16:56:12 -0700

    Update merge_2_arr_unique.py

--- merge_2_arr_unique.py
+++ merge_2_arr_unique.py
@@ -59,3 +59,47 @@ print(f"Unsorted list is {a}")
 msort(a)
 
 print(f"Sorted list with unique elements is {a}")
+
+
+"""
+assuming that a and b are sorted already
+solution time complexity is O(m + n), 
+space complexity is 
+O(m) when m >> n
+O(n) when n >> m
+"""
+def merge_sort_dedupe(a,b):
+    merged = list()
+    i, j = 0,0
+
+    while i < len(a) and j < len(b):
+        # compare each element
+        if a[i] < b[j]:
+            # check if last element is equal to current
+            # to avoid duplicates
+            if len(merged)==0 or len(merged) > 0 and merged[-1] != a[i]:
+                merged.append(a[i])
+            i+=1
+        else:
+            if len(merged) == 0 or len(merged) > 0 and merged[-1] != b[j]:
+                merged.append(b[j])
+            j+=1
+    # add the rest
+    while i < len(a):
+        if merged[-1] != a[i]:
+            merged.append(a[i])
+        i+=1
+
+    while j < len(b):
+        if  merged[-1] != b[j]:
+            merged.append(b[j])
+        j+=1
+    return merged
+
+
+# driver code
+m = [2, 6, 6, 7, 12, 17,17,17, 19,19,19, 26, 50]
+n = [7, 9, 11, 14, 14, 26]
+#print(intersect(m,n))
+
+print(merge_sort_dedupe(m,n))
