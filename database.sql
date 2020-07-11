f1949dec768924abd3b62cc3c17fdb243fb70d4e
Khazret Sapenov <sapenov@users.noreply.github.com>
2020-07-10 21:16:01 -0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -38,3 +38,42 @@ inner join (
 	from orders group by month 
 ) o 
 on c.month = o.month
+
+// Conversations Distribution
+/*
+We have a table that represents the total number of messages sent between two users by date on messenger.
+
+1. What are some insights that could be derived from this table?
+
+2. What do you think the distribution of the number of conversations created by each user per day looks like?
+
+3. Write a query to get the distribution of the number of conversations created by each user by day in the year 2020.
+*/
+
+SELECT num_conversations, COUNT(*) AS frequency
+FROM (
+    SELECT user1, DATE(created_at), COUNT(DISTINCT user2) AS num_conversations
+    FROM messages
+    WHERE YEAR(date) = '2020'
+    GROUP BY 1
+) AS t
+GROUP BY 1
+
+// Manager Team Sizes
+// Write a query to identify managers with the biggest team size.
+WITH t AS(
+SELECT 
+	m.id AS manager_id,
+	m.name AS manager_name,
+       COUNT(DISTINCT e.id)  AS total_employees
+FROM
+	Employees m
+LEFT JOIN
+	Managers m
+ON 
+	m.id = e.manager_id
+AND
+	m.id IS NOT NULL
+GROUP BY m.id
+) SELECT  manager_id, manager_name FROM t ORDER BY total_employees DESC LIMIT 5;
+
